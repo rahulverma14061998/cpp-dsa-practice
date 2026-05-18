95f0e0de2c0ca3b09e044cf6e1a4e6d6d7533eeb
Rahul Verma <128915643+rahulverma14061998@users.noreply.github.com>
2026-05-18 13:02:56 +0530

    Add longestCommonPrefix function in Solution class
    Implement function to find longest common prefix in an array of strings.

---
+++ Strings/Longest_Common_Prefix.cpp
@@ -0,0 +1,19 @@
+// User function template for C++
+class Solution {
+  public:
+    string longestCommonPrefix(vector<string> arr) {
+        // your code here
+        string ans = "";
+        for(int i=0;i<arr[0].size();i++){
+            char ch = arr[0][i];
+            
+            for(int j=1;j<arr.size();j++){
+                if(i>=arr[j].size() || arr[j][i]!=ch){
+                    return ans;
+                }
+            }
+                ans += ch;
+        }
+        return ans;
+    }
+};
